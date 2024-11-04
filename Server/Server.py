8b323bd8375c0c2d2faa36752f5673a3fa68d4f6
StevenTEC295 <165940741+StevenTEC295@users.noreply.github.com>
2024-11-03 22:57:50 -0600

    Corrección server

--- Server/Server.py
+++ Server/Server.py
@@ -184,9 +184,9 @@ class Server:
             print(filename)
             data = self.returnHouse(filename)
 
-            all_data.append(data.encode().decode('unicode_escape'))
-        print(all_data)
-        clean_data = str(all_data).encode().decode('unicode_escape')
+            all_data.append(data.encode().decode('utf-8'))
+        #print(all_data)
+        clean_data = str(all_data).encode().decode('utf-8')
         print(clean_data)
         #client_socket.sendall(clean_data.encode("utf-8"))
         #msj = "Hola"
@@ -211,5 +211,14 @@ class Server:
             client.close()
         self.server_socket.close()
         self.root.destroy()
+        
+    def read_arduino_msg(self):# Recibe el mensaje del flame, enviado desde el Arduino
+        while True:
+            ino_message = self.arduino.read_until(b"\n").decode('utf-8')
+            self.broadcast1(ino_message, self.server_socket)
+        
+   
+            
+            
 if __name__ == "__main__":
     Server()
