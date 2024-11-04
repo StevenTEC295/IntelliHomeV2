3e37d1c1ca1af7dc3abe6370fa6ec164e8e6ee07
StevenTEC295 <165940741+StevenTEC295@users.noreply.github.com>
2024-11-03 23:27:30 -0600

    Comunicación con el Arduino

--- Server/ArduinoConnection.py
+++ Server/ArduinoConnection.py
@@ -13,7 +13,10 @@ class ArduinoConnection:
 
     def receive(self):
         return self.arduino.readline().decode()
+    
+    def read(self):
+        return self.arduino.read().decode()
         
     def close(self):
         self.arduino.close()
-        
+    
