07f9c2244a20d7ecdfcfcec28303efbab5913295
PabloRamosMGit <80283955+PabloRamosMGit@users.noreply.github.com>
2024-11-07 19:49:34 -0600

    Añádido de nuevo la barra y la notifi

--- app/src/main/java/com/example/IntelliHome/Constants.kt
+++ app/src/main/java/com/example/IntelliHome/Constants.kt
@@ -2,11 +2,13 @@ package com.example.IntelliHome
 
 object Constants {
     const val SERVER_IP = "192.168.0.118"
-    const val SERVER_PORT = 8081
+    const val SERVER_PORT = 8080
     const val REGISTRO = "registro"
     const val LOGIN = "login"
     const val ARDUINO = "arduino"
     const val RQHOUSE = "rq_house"
     const val SVHOUSE = "sv_house"
     const val ABANQUERO = "a_Banquero"
+    const val NOTIFICASA = "noti_casa_alquilada"
+
 }
